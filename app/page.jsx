0d5663e6dfdcf4cd445ae2401137483c1046dae7
woongbaera <woongbaera@gmail.com>
2023-09-20 03:15:11 +0900

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="scale-150 font-serif text-xl text-white">2023.10 Grand Open</div>
+    </main>
+  );
+}
