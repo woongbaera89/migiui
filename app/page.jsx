1f608a93fb35a50c8977c53a2dfa53ba8498fc8c
woongbaera <woongbaera@gmail.com>
2023-09-20 04:22:17 +0900

    responsive global size

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <main className="flex relative min-h-screen flex-col items-center justify-center">
       <Image
-        className="absolute right-0 top-0 m-16"
+        className="absolute right-0 top-0 w-16 m-8"
         src="/logo.svg"
         alt="home"
         width={120}
         height={360}
       />
-      <div className="absolute left-0 bottom-0 m-16 font-serif text-6xl text-white whitespace-pre-line">{`2023.10\nGrand Open`}</div>
+      <div className="absolute left-0 bottom-0 font-serif text-3xl m-8 text-white whitespace-pre-line">{`2023.10\nGrand Open`}</div>
     </main>
   );
 }
